82f35750525b2dc2af7a8e2e9fe553386f6273ce
Max <maxpete121@gmail.com>
2024-01-22 10:54:40 -0700

    stock parts

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-0mzshsqy0abngajt.us.auth0.com'
+export const clientId = 'UqJENWmOBgBlQb9arIAhk358eP9IiZcN'
+export const audience = 'https://TestApi.com'
+
+
