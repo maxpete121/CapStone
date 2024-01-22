32b0a595416926a9779f9afec0853342b5b40a36
Max <maxpete121@gmail.com>
2024-01-22 15:46:35 -0700

    update

--- server/src/models/StockPart.js
+++ server/src/models/StockPart.js
@@ -8,10 +8,11 @@ export const StockPartSchema = new Schema({
     productImage: {type: String, maxLength: 500, required: true},
     type: {type: String, enum: ['cpu','gpu','motherboard','ram', 'storage', 'cpuCooler', 'powerSupply', 'case', 'caseFan' ], required: true},
     size: {type: String, enum: ['atx', 'micro-atx', 'itx', 'none'], required: true},
+    chipSet: {type: String, enum:['AM5', '14th Gen']},
     price: {type: Number, required: true, minLength: 2},
-    name: {type: String, required: true, maxLength: 30, minLength: 4},
-    company: {type: String, required: true, maxLength: 15, minLength: 3},
-    speed: {type: String, maxLength: 10, minLength: 2},
-    gigs: {type: String, maxLength: 15, minLength: 2},
+    name: {type: String, required: true, maxLength: 50, minLength: 4},
+    company: {type: String, required: true, maxLength: 15},
+    speed: {type: String, maxLength: 10},
+    gigs: {type: String, maxLength: 15},
     powerScore: {type: Number, maxLength: 7, minLength: 1, required: true}
 })
