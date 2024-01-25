4e013654fb025dd2fa31cd4a002ff7baa6afd017
Max <maxpete121@gmail.com>
2024-01-25 09:54:11 -0700

    conflict

--- client/src/models/StockPart.js
+++ client/src/models/StockPart.js
@@ -1,7 +1,15 @@
 
-
-
-
-export class StockPart{
-    
+export class StockPart {
+    constructor(data) {
+        this.productImage = data.productImage
+        this.type = data.type
+        this.size = data.size
+        this.chipSet = data.chipSet
+        this.price = data.price
+        this.name = data.name
+        this.company = data.company
+        this.speed = data.speed
+        this.gigs = data.gigs
+        this.powerScore = data.powerScore
+    }
 }
