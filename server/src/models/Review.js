276e2b246a7e02ff9ea46115411c1be99927ef2f
Skyler Kinney <Skylerjkinney@gmail.com>
2024-01-30 12:04:45 -0700

    reviews works

--- server/src/models/Review.js
+++ server/src/models/Review.js
@@ -3,8 +3,8 @@ import { Schema } from "mongoose";
 export const ReviewSchema = new Schema({
     body: { type: String, required: true, maxlength: 500 },
     creatorId: { type: Schema.Types.ObjectId, required: true, ref: 'Account' },
-    pcId: {type: Schema.Types.ObjectId, required: true},
-    rating: {type: Number, maxLength: 1, required: true}
+    pcId: { type: Schema.Types.ObjectId },
+    rating: { type: Number, maxLength: 1 }
 },
     {
         timestamps: true,
