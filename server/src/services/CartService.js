a38f4f860cd0f5f4af924df20ad82290ded888d9
Max <maxpete121@gmail.com>
2024-01-30 15:34:28 -0700

    cart base

---
+++ server/src/services/CartService.js
@@ -0,0 +1,12 @@
+import { dbContext } from "../db/DbContext.js"
+
+
+
+
+class CartService{
+    async getCartItems(accountId){
+        let found = await dbContext
+    }
+}
+
+export const cartService = new CartService()
